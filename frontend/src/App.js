f2ddaaf45648e05b1f2507e23046dea002578b80
Harddancer <yamamotod@mail.ru>
2022-08-15 13:27:16 +0300

    correct frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import UserList from './components/User.js'
+
 class App extends React.Component {
 constructor(props) {
 super(props)
