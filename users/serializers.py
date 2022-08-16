1f24eaed368e068105f062a4239cae8273a2bbb7
Harddancer <yamamotod@mail.ru>
2022-08-16 18:54:55 +0300

    HW3Done

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,21 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Users
 
-class UserModelSerializer(ModelSerializer):
-    class Meta:
-        model = Users
-        fields = '__all__'
+from rest_framework import serializers
+from .models import Users,Project,TODO
+
+class UserSerializer(serializers.Serializer):
+   address = serializers.CharField(max_length=30, blank=True)
+   name_user = serializers.CharField(max_length=64)
+
+class ProjectSerializer(serializers.Serializer):
+    name_project = serializers.CharField(max_length=64)
+    user = UserSerializer(many=True)
+
+class TODOSerializer(serializers.Serializer):
+    name_todo = serializers.CharField(max_length=64)
+    text = serializers.CharField(max_length=64)
+    date_creation = serializers.DateField
+    date_update = serializers.DateField
+    name_user = serializers.CharField(max_length=64)
+    status = serializers.CharField(max_length=64)
+    user = UserSerializer()
+
