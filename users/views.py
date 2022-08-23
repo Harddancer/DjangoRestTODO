a3356b8ef5a0a98a1688880902cac7e469d991e5
Harddancer <yamamotod@mail.ru>
2022-08-23 20:59:36 +0300

    HW4_5

--- users/views.py
+++ users/views.py
@@ -1,6 +1,17 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+
 from .models import Users
 from .serializers import UserModelSerializer
+
+
 class UserModelViewSet(ModelViewSet):
-    queryset = Users.objects.all()
     serializer_class = UserModelSerializer
+    queryset = Users.objects.all()
+
+
+class UserModelLimitedViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.UpdateModelMixin, GenericViewSet
+):
+    serializer_class = UserModelSerializer
+    queryset = Users.objects.all()
