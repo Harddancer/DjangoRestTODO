c33497f7ffe63b361055beaed75cc22d9c8ff9a2
Harddancer <yamamotod@mail.ru>
2022-09-24 12:20:53 +0300

    HW12

---
+++ config/settings/debug_pq.py
@@ -0,0 +1,12 @@
+from .debug import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TODO',
+        'USER': 'yamamotod',
+        'PASSWORD': '88888888',
+        'HOST': '127.0.0.1',
+        'PORT': '54328',
+    }
+}
